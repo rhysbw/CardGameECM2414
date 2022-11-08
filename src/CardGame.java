1e0c89dd80308d6767848696404d86d629f0fa52
rhys b <rhyswardbroughton@gmail.com>
2022-11-08 11:22:23 +0000

    packClass can now import a packfile (*.txt)

--- src/CardGame.java
+++ src/CardGame.java
@@ -1,8 +1,14 @@
+import java.util.Arrays;
 
 public class CardGame {
     CardGame(){
-
     }
-    public static void main(String[] args){}
+    public static void main(String[] args){
+        // TEST CODE TO CHECK PACK IMPORTING WORKS
+        Pack testPack = new Pack("testPack.txt", 1);
+        for (Card c : testPack.getCards()){
+            System.out.println(c.getCardValue());
+        }
+    }
 
 }
--- src/Pack.java
+++ src/Pack.java
@@ -1,4 +1,7 @@
 import java.util.ArrayList;
+import java.io.File;  // Import the File class
+import java.io.FileNotFoundException;  // Import this class to handle errors
+import java.util.Scanner; // Import the Scanner class to read text files
 
 public class Pack {
     private ArrayList<Card> cards;
@@ -7,12 +10,27 @@ public class Pack {
     Pack(String filename, int numberOfPLayers){
         this.numberOfPlayers = numberOfPLayers;
         this.filename = filename;
+        this.cards = new ArrayList<Card>();
         importPack();
     }
 
-    private void importPack(){}
-    private ArrayList getCards(){
-        return null;
+    private void importPack(){
+
+        try{
+            File packFile = new File(filename);
+            Scanner myReader = new Scanner(packFile);
+            while (myReader.hasNextLine()){
+                String data = myReader.nextLine();
+                cards.add(new Card(Integer.parseInt(data))); // makes a new card from int in packfile
+            }
+            myReader.close();
+        }catch (FileNotFoundException e){
+            System.out.println("File not found");
+        }
+
+    }
+    public ArrayList<Card> getCards(){
+        return cards;
     }
 
     public int getNumberOfPlayers() {
@@ -22,4 +40,5 @@ public class Pack {
     public String getFilename() {
         return filename;
     }
+
 }
